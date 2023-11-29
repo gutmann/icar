251cbb2f1d3c229de1101c02a38b5a33d979bf14
Ethan Gutmann <gutmann@ucar.edu>
2023-11-29 07:44:19 -0700

    change inverted pressure error stop in rrtmg_LW to a warning

--- src/physics/ra_rrtmg_lw.f90
+++ src/physics/ra_rrtmg_lw.f90
@@ -2392,7 +2392,7 @@ contains
 ! Must use pmid from bottom four layers.
          do i=1,ncol
             if (pmid(i,1).lt.pmid(i,2)) then
-               stop 'MCICA_SUBCOL: KISSVEC SEED GENERATOR REQUIRES PMID FROM BOTTOM FOUR LAYERS.'
+               print* 'WARNING: MCICA_SUBCOL: KISSVEC SEED GENERATOR REQUIRES PMID FROM BOTTOM FOUR LAYERS.'
             endif
             seed1(i) = (pmid(i,1) - int(pmid(i,1)))  * 1000000000_im
             seed2(i) = (pmid(i,2) - int(pmid(i,2)))  * 1000000000_im
